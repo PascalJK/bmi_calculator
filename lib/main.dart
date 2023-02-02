0a3db076461b15429d0c2bc55db3d1cce075bd03
Pascal JK <pascual.santos80@gmail.com>
2023-02-02 18:51:37 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'pages/input_page.dart';
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF0a0e21),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF0a0e21),
+          centerTitle: true,
+        ),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red,
+          accentColor: Colors.purple,
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: const InputPage(),
+    );
+  }
+}
+
