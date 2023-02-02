54c696402012a07783910b43cf4e375557cb6fcc
Pascal JK <pascual.santos80@gmail.com>
2023-02-02 20:22:14 +0200

    - ui design.

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const Color activeCardColor = Color(0xFF1d1e33);
+const double bottomContainerHeight = 80.0;
+const Color bottomContainerColor = Colors.purple;
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -14,12 +18,41 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                _reusableCardWidget(),
+                _reusableCardWidget(),
+              ],
+            ),
+          ),
+          _reusableCardWidget(),
+          Expanded(
+            child: Row(
+              children: [
+                _reusableCardWidget(color: Colors.black12),
+                _reusableCardWidget(),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            height: bottomContainerHeight,
+            margin: const EdgeInsets.only(top: 10),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Expanded _reusableCardWidget({Color color = activeCardColor}) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10)),
       ),
     );
   }
